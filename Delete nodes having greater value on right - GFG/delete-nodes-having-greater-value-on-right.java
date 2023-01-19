f1adacb9624f52d21b1fa6484508a5221e349e3b
Deepak-000 <84334673+Deepak-000@users.noreply.github.com>
2023-01-19 23:26:03 +0530

    Added solution - LeetHub

---
+++ Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+        // your code here
+        List<Integer>li = new ArrayList<>();
+        Node temp = head;
+        while(temp!=null){
+            li.add(temp.data);
+            temp = temp.next;
+        }
+        int n = li.size();
+        temp = null;
+        head = null;
+        int mx = li.get(n-1);
+        List<Integer>res = new ArrayList<>();
+        res.add(mx);
+        for(int i = n-2 ; i>=0 ; i--){
+            if(li.get(i) >= mx){
+                res.add(li.get(i));
+                mx = li.get(i);
+            }
+        }
+        Collections.reverse(res);
+        for(int i = 0 ; i < res.size() ; i++){
+            if(i == 0)
+                head = temp = new Node(res.get(i));
+            else{
+                temp.next = new Node(res.get(i));
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
+  
