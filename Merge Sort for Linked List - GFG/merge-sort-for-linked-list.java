c68e4275e04b818de842cae6a6cc375628d73536
Deepak-000 <84334673+Deepak-000@users.noreply.github.com>
2023-01-19 23:32:39 +0530

    Added solution - LeetHub

---
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.java
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+        // add your code here
+        Node  temp = head ; 
+        List<Integer>li = new ArrayList<>();
+        while(temp!=null){
+            li.add(temp.data);
+            temp = temp.next;
+        }
+        head = null;
+        temp = null;
+        Collections.sort(li);
+        for(int i = 0 ; i < li.size() ; i++){
+            if(i==0)
+                head = temp = new Node(li.get(i));
+            else{
+                temp.next = new Node(li.get(i));
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
+
+
