d5e9f6e907f83161e182d3269af51f57d195031c
Ajay Keelu <84334673+Deepak-000@users.noreply.github.com>
2023-04-24 21:04:01 +0530

    Added solution - LeetHub

--- Equilibrium Point - GFG/equilibrium-point.java
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -39,23 +39,22 @@ class Solution {
     // n: size of array
     // Function to find equilibrium point in the array.
     public static int equilibriumPoint(long arr[], int n) {
-
-        // Your code here
-        long pre[] = new long[n];
-        pre[0] = arr[0];
-        for(int i=1;i<n;i++)
-        {
-            pre[i] = pre[i-1]+arr[i];
+            long pre[] = new long[n];
+        long s = 0;
+        for(int i = 0 ; i < n ; i++){
+            s += arr[i];
+            pre[i] = s;
         }
-        if(pre[n-1]-pre[0]==0)
-        return 1;
-        for(int i=1;i<n-1;i++)
-        {
+        if(n == 1)
+            return 1;
+        if(pre[n-1]-pre[0] == 0)
+            return 1;
+        for(int i = 1 ; i < n-1 ; i++){
             if(pre[n-1]-pre[i] == pre[i-1])
-            return i+1;
+                return i+1;
         }
-        if(pre[n-2]==0)
-        return n;
+        if(pre[n-2] == 0)
+            return n-1;
         return -1;
     }
 }
