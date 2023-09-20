8a9dc00aee51687d41a57677aa63f7797be17af4
Ajay Keelu <84334673+ajay-keelu@users.noreply.github.com>
2023-09-20 23:36:07 +0530

    Added solution - LeetHub

---
+++ Rotate Bits - GFG/rotate-bits.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int d = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+            ArrayList<Integer> res = ob.rotate (n, d);
+            System.out.println(res.get(0));
+            System.out.println(res.get(1));
+            
+           
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    ArrayList<Integer> rotate(int n, int d)
+    {
+        ArrayList<Integer> res = new ArrayList<>();
+        d = d%16;
+        res.add( ( (n << d) | ( n >> (16-d) ) ) & 0xFFFF );
+        res.add( ( (n >> d) | ( n << (16-d) ) ) & 0xFFFF );
+        return res;
+    }
+}
